fc66218dd2682b6a0caae5b3070f54596d9375ea
Francis Yuweh <francisyuweh@CPUZ132.local>
2020-07-14 17:30:24 +0800

    initial setup added

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Francis Yuweh on 7/14/20.
+//  Copyright © 2020 Francis XiApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
